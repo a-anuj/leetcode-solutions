77a4ad7cd924351d74a1d542926d7d9605611646
Anuj A <anujamirthalingam5@gmail.com>
2025-11-23 16:46:37 +0530

    Time: 0 ms (100%), Space: 12.7 MB (20.1%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        return list(permutations(nums))
