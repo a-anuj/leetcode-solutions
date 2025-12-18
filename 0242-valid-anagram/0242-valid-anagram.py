4dbbcdf28883aa778ff69299f21880ff3953cd3d
Anuj A <anujamirthalingam5@gmail.com>
2025-12-18 07:53:23 +0530

    Time: 7 ms (90.04%), Space: 17.9 MB (47.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,5 @@
 from collections import Counter
 
-class Solution(object):
-    def isAnagram(self, s, t):
-        # lstS = list(s)
-        # lstT = list(t)
-
-        # for i in lstT:
-        #     if i in lstS:
-        #         lstS.remove(i)
-        #     else:
-        #         return False
-        # return len(lstS)==0
-
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         return Counter(s) == Counter(t)
