f058c544a2b6bf92d173c35a5082a72b1d99f33e
Anuj A <anujamirthalingam5@gmail.com>
2026-04-11 08:34:54 +0530

    Time: 32 ms (65.55%), Space: 21.7 MB (88.87%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,14 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         prefixSum = 0
-        map1 = {0:1}
+        mapCount = {0:1}
         count = 0
 
         for num in nums:
             prefixSum += num
 
-            if prefixSum-k in map1:
-                count += map1[prefixSum-k]
-
-            map1[prefixSum] =  map1.get(prefixSum,0)+1
+            if prefixSum - k in mapCount:
+                count += mapCount[prefixSum-k] 
+            
+            mapCount[prefixSum] = mapCount.get(prefixSum,0) + 1
+        
         return count
