d5a933870536074470469b6bdb58635084e3d4ed
Anuj A <anujamirthalingam5@gmail.com>
2026-08-28 12:56:02 +0530

    Time: 1 ms (14.71%), Space: 19.4 MB (23.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,18 +1,20 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         left = 0
-        right = len(nums)-1
         ptr = 0
+        right = len(nums) - 1
 
-        while ptr<=right:
+        while ptr <= right:
             if nums[ptr] == 0:
                 nums[ptr],nums[left] = nums[left],nums[ptr]
-                ptr+=1
-                left+=1
+                ptr += 1
+                left += 1
+            
             elif nums[ptr] == 1:
                 ptr+=1
             
-            else:
+            elif nums[ptr] == 2:
                 nums[ptr],nums[right] = nums[right],nums[ptr]
-                right-=1
+                right -= 1
+
         return nums
